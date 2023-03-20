581a681d43ffd5dd92d24ab09f00c72da9fe27b1
Sacha STAFYNIAK <sacha@digisquad.io>
2023-03-20 14:19:55 +0100

    feat: upgrade dependencies + add unplugin-fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import Unfonts from 'unplugin-fonts/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    Unfonts({
+      google: {
+        families: [
+          'Inter',
+        ],
+      },
+    })
+  ]
 });
